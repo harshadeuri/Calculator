c67ef95ee29ee4f0b754031fdbb064ed77c5e7e9
harshadeuri <harsha.deuri@st.niituniversity.in>
2019-09-25 23:13:11 +0530

    First commit

---
+++ calc.py
@@ -0,0 +1,21 @@
+print("MENU")
+print("1. ADD")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+choice=int(input("Make a choice"))
+
+
+if choice==1:
+	ADD()
+	
+elif choice==2:
+	Subtract()
+		
+elif choice==3:
+	Multiply()
+	
+elif choice==4:
+	Divide()
+		
+
