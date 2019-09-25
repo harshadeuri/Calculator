43c33af7eeab911d46a986e941016b3365c1f2ee
harshadeuri <harsha.deuri@st.niituniversity.in>
2019-09-25 23:29:17 +0530

    Third commit

--- calc.py
+++ calc.py
@@ -1,23 +1,27 @@
+def fnc(choice):
+	a=int(input("Enter first number"))
+	b=int(input("Enter second number"))
+	if choice==1:
+		ADD(a,b)
+		
+	elif choice==2:
+		Subtract(a,b)
+			
+	elif choice==3:
+		Multiply(a,b)
+		
+	elif choice==4:
+		Divide(a,b)
+		
+
+
+
 print("MENU")
 print("1. ADD")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 choice=int(input("Make a choice"))
-a=int(input("Enter first number"))
-b=int(input("Enter second number"))
+fnc(choice)
 
-
-if choice==1:
-	ADD(a,b)
-	
-elif choice==2:
-	Subtract(a,b)
-		
-elif choice==3:
-	Multiply(a,b)
-	
-elif choice==4:
-	Divide(a,b)
-		
 
